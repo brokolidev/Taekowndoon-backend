8588646ecfe6a83fcf8a3ae8ec8e58f2f2535874
brokolidev <bocalist@gmail.com>
2024-12-03 13:52:47 -0700

    added login api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Requests\Auth\LoginRequest;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::post('/login', function (LoginRequest $request) {
+    $request->authenticate();
+
+    $user = Auth::user();
+
+    $token = $user->createToken('api-token')->plainTextToken;
+
+    return ['token' => $token];
 });
