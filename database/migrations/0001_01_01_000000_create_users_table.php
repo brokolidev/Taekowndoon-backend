81c3ebce7605410a800ae90e77c4243961bf9d83
brokolidev <bocalist@gmail.com>
2024-12-04 14:07:06 -0700

    added profile url column for users

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -18,6 +18,7 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->string('profile_img')->default('https://i.pravatar.cc/300')->nullable();
             $table->enum('belt_color', [
                 'white', 'yellow',
                 'green', 'blue',
