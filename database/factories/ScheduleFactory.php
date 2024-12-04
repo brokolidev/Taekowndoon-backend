cd8fe2ad3bd5510a0b348444a6de787c2e48de6d
brokolidev <bocalist@gmail.com>
2024-12-04 15:16:05 -0700

    added schedule seeder

--- database/factories/ScheduleFactory.php
+++ database/factories/ScheduleFactory.php
@@ -2,6 +2,7 @@
 
 namespace Database\Factories;
 
+use App\Models\Timeslot;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
@@ -19,12 +20,9 @@ class ScheduleFactory extends Factory
     public function definition(): array
     {
         return [
-            'first_name' => fake()->name(),
-            'last_name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'dow' => fake()->name(),
+            'level' => fake()->name(),
+            'timeslot_id' => Timeslot::factory(),
         ];
     }
 }
--- database/seeders/ScheduleSeeder.php
+++ database/seeders/ScheduleSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Schedule;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -12,6 +13,105 @@ class ScheduleSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+
+        // classes for SAT
+        Schedule::factory()->create([
+            'timeslot_id' => 1,
+            'dow' => 6,
+            'level' => 'Little Warriors'
+        ]);
+
+        Schedule::factory()->create([
+            'timeslot_id' => 2,
+            'dow' => 6,
+            'level' => 'Beginner Class'
+        ]);
+
+        Schedule::factory()->create([
+            'timeslot_id' => 3,
+            'dow' => 6,
+            'level' => 'Family Class'
+        ]);
+
+        Schedule::factory()->create([
+            'timeslot_id' => 4,
+            'dow' => 6,
+            'level' => 'Private Class'
+        ]);
+
+        // classes for 3:30 ~ 4:15
+        Schedule::factory()->create([
+            'timeslot_id' => 5,
+            'dow' => 2,
+            'level' => 'Private Lesson'
+        ]);
+
+        Schedule::factory()->create([
+            'timeslot_id' => 5,
+            'dow' => 3,
+            'level' => 'Private Lesson'
+        ]);
+
+        Schedule::factory()->create([
+            'timeslot_id' => 5,
+            'dow' => 4,
+            'level' => 'Private Lesson'
+        ]);
+
+        Schedule::factory()->create([
+            'timeslot_id' => 5,
+            'dow' => 5,
+            'level' => 'Private Lesson'
+        ]);
+
+        // classes for 4:30 ~ 5:00
+        Schedule::factory()->create([
+            'timeslot_id' => 6,
+            'dow' => 2,
+            'level' => 'Little Warriors 1'
+        ]);
+
+        Schedule::factory()->create([
+            'timeslot_id' => 6,
+            'dow' => 3,
+            'level' => 'Little Warriors 1'
+        ]);
+
+        Schedule::factory()->create([
+            'timeslot_id' => 6,
+            'dow' => 4,
+            'level' => 'Little Warriors 1'
+        ]);
+
+        Schedule::factory()->create([
+            'timeslot_id' => 6,
+            'dow' => 5,
+            'level' => 'Little Warriors 1'
+        ]);
+
+        // classes for 5:00 ~ 5:40
+        Schedule::factory()->create([
+            'timeslot_id' => 7,
+            'dow' => 2,
+            'level' => 'Beginner Class 1'
+        ]);
+
+        Schedule::factory()->create([
+            'timeslot_id' => 7,
+            'dow' => 3,
+            'level' => 'Beginner Class 1'
+        ]);
+
+        Schedule::factory()->create([
+            'timeslot_id' => 7,
+            'dow' => 4,
+            'level' => 'Beginner Class 1'
+        ]);
+
+        Schedule::factory()->create([
+            'timeslot_id' => 7,
+            'dow' => 5,
+            'level' => 'Beginner Class 1'
+        ]);
     }
 }
