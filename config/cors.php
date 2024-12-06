a138abab17d1f85755220d770f03ad678bdae846
brokolidev <bocalist@gmail.com>
2024-12-05 21:35:57 -0700

    added front production domain to allow api calls

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,11 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => [env('FRONTEND_URL', 'http://localhost:3000')],
+    'allowed_origins' => [
+        env('FRONTEND_URL', 'http://localhost:3000'),
+        'https://*.brokolidev.com',
+        'https://tkdapp.brokolidev.com',
+    ],
 
     'allowed_origins_patterns' => [],
 
